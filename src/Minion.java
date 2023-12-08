cba005b2d3b21f2281adb37a85d546237491c7bd
DeZoete <143606680+DeZoete@users.noreply.github.com>
2023-12-08 11:41:07 +0100

    Joans work

--- src/Minion.java
+++ src/Minion.java
@@ -1,2 +1,12 @@
 public class Minion {
+
+    int minionAttack;
+    int minionCurrentHealth;
+    int minionMaxHealth;
+    minionEffect effect;
+
+    void setMinionHealth(int health){
+
+
+    }
 }
