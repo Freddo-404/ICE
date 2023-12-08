0569272ad78f3da9640ac81e6e7bc82e66950c46
DeZoete <143606680+DeZoete@users.noreply.github.com>
2023-12-08 12:23:47 +0100

    i am inside your walls

--- src/MinionEffect.java
+++ src/MinionEffect.java
@@ -1,2 +1,5 @@
 public class  MinionEffect extends Minion {
+    public MinionEffect(String cardName, int cardCost, int minionAttack, int minionCurrentHealth, int minionMaxHealth, MinionEffect effect) {
+        super(cardName, cardCost, minionAttack, minionCurrentHealth, minionMaxHealth, effect);
+    }
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -1,6 +1,22 @@
-public class Weapon {
+public class Weapon extends Card{
     private int weaponAttack;
     private int weaponDurability;
     private WeaponEffect effect;
 
+
+    public Weapon(String cardName, int cardCost, int weaponAttack, int weaponDurability, WeaponEffect effect) {
+        super(cardName, cardCost);
+    }
+
+    public int getWeaponAttack() {
+        return weaponAttack;
+    }
+    public int getWeaponDurability() {
+        return weaponDurability;
+    }
+
+
+    public WeaponEffect getEffect() {
+        return effect;
+    }
 }
