b4fe832daf16632c06808820ff0e3b7a091140d2
Marcus-Ekeroth <cph-me313@cphbusiness.dk>
2023-12-11 12:46:05 +0100

    merging time

--- src/Deck.java
+++ src/Deck.java
@@ -4,10 +4,6 @@ public class Deck {
 
    private int fatigueCount;
    private FileIO io = new FileIO();
-   private LinkedList<Card> allWeapons = new LinkedList<>();
-   private LinkedList<Card> allMinions = new LinkedList<>();
-   private LinkedList<Card> allSpells = new LinkedList<>();
-
    private LinkedList<Card> cardsInDeck = new LinkedList<>();
 
    public Deck(LinkedList<Card> cardsInDeck){
@@ -18,17 +14,4 @@ public LinkedList<Card> getCardsInDeck(){
 }
 
 
-   public LinkedList<Card> getAllMinions(){
-      allMinions = io.readMinionData("card.txt");
-      return allMinions;
-   }
-
-   public LinkedList<Card> getAllWeapons(){
-      allWeapons = io.readWeaponData("weapons.txt");
-      return allWeapons;
-   }
-   public LinkedList<Card> getAllSpells(){
-      allSpells = io.readSpellData("spells.txt");
-      return allSpells;
-   }
 }
