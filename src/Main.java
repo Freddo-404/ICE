ba68c5ac10ccb1d5345b5e8f32b0ec417127bb7c
Freddo-404 <cph-fd101@cphbusiness.dk>
2023-12-14 14:48:44 +0100

    Nu med musik

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+
 public class Main {
 
     public static void main(String[] args) {
         Game game = new Game();
-        game.mainMenu();
-        }
-
+        game.startUp();
     }
+}
