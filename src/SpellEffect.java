fbac2e6c61b46f148a086ffe3a44f0cee54e5831
DeZoete <143606680+DeZoete@users.noreply.github.com>
2023-12-15 16:42:12 +0100

    Hero power update

--- src/SpellEffect.java
+++ src/SpellEffect.java
@@ -38,7 +38,7 @@ public class SpellEffect extends Effect {
                 Random ran = new Random();
                 int ranNum;
 
-                for (int i = 0; i < 3 ; i++) {
+                for (int i = 0; i < 3; i++) {
                     ranNum = ran.nextInt(enemyBoard.getMinionsOnBoard().size() + 1);
                     switch (ranNum) {
                         case 0:
@@ -46,31 +46,31 @@ public class SpellEffect extends Effect {
                             break;
                         case 1:
                             enemyBoard.getMinionsOnBoard().get(0).loseHealth(1);
-                            enemyBoard.getMinionsOnBoard().get(0).minionDeath(enemyBoard.getMinionsOnBoard().get(0),enemyBoard.getMinionsOnBoard());
+                            enemyBoard.getMinionsOnBoard().get(0).minionDeath(enemyBoard.getMinionsOnBoard().get(0), enemyBoard.getMinionsOnBoard());
                             break;
                         case 2:
                             enemyBoard.getMinionsOnBoard().get(1).loseHealth(1);
-                            enemyBoard.getMinionsOnBoard().get(1).minionDeath(enemyBoard.getMinionsOnBoard().get(1),enemyBoard.getMinionsOnBoard());
+                            enemyBoard.getMinionsOnBoard().get(1).minionDeath(enemyBoard.getMinionsOnBoard().get(1), enemyBoard.getMinionsOnBoard());
                             break;
                         case 3:
                             enemyBoard.getMinionsOnBoard().get(2).loseHealth(1);
-                            enemyBoard.getMinionsOnBoard().get(2).minionDeath(enemyBoard.getMinionsOnBoard().get(2),enemyBoard.getMinionsOnBoard());
+                            enemyBoard.getMinionsOnBoard().get(2).minionDeath(enemyBoard.getMinionsOnBoard().get(2), enemyBoard.getMinionsOnBoard());
                             break;
                         case 4:
                             enemyBoard.getMinionsOnBoard().get(3).loseHealth(1);
-                            enemyBoard.getMinionsOnBoard().get(3).minionDeath(enemyBoard.getMinionsOnBoard().get(3),enemyBoard.getMinionsOnBoard());
+                            enemyBoard.getMinionsOnBoard().get(3).minionDeath(enemyBoard.getMinionsOnBoard().get(3), enemyBoard.getMinionsOnBoard());
                             break;
                         case 5:
                             enemyBoard.getMinionsOnBoard().get(4).loseHealth(1);
-                            enemyBoard.getMinionsOnBoard().get(4).minionDeath(enemyBoard.getMinionsOnBoard().get(4),enemyBoard.getMinionsOnBoard());
+                            enemyBoard.getMinionsOnBoard().get(4).minionDeath(enemyBoard.getMinionsOnBoard().get(4), enemyBoard.getMinionsOnBoard());
                             break;
                         case 6:
                             enemyBoard.getMinionsOnBoard().get(5).loseHealth(1);
-                            enemyBoard.getMinionsOnBoard().get(5).minionDeath(enemyBoard.getMinionsOnBoard().get(5),enemyBoard.getMinionsOnBoard());
+                            enemyBoard.getMinionsOnBoard().get(5).minionDeath(enemyBoard.getMinionsOnBoard().get(5), enemyBoard.getMinionsOnBoard());
                             break;
                         case 7:
                             enemyBoard.getMinionsOnBoard().get(6).loseHealth(1);
-                            enemyBoard.getMinionsOnBoard().get(6).minionDeath(enemyBoard.getMinionsOnBoard().get(6),enemyBoard.getMinionsOnBoard());
+                            enemyBoard.getMinionsOnBoard().get(6).minionDeath(enemyBoard.getMinionsOnBoard().get(6), enemyBoard.getMinionsOnBoard());
                             break;
                         default:
                             ui.displayMessage("Something went wrong with ranNum in Arcane Missiles.");
@@ -103,43 +103,84 @@ public class SpellEffect extends Effect {
                 ui.displayMessage("Your target is hit by a mighty Pyroblast and loses 10 hp. Rest in peace.");
                 break;
             case "Flamestrike":
-                     for(Minion m : enemyBoard.getMinionsOnBoard()){
-                         m.loseHealth(4);
-                     }
+                for (Minion m : enemyBoard.getMinionsOnBoard()) {
+                    m.loseHealth(4);
+                }
 
-                    for(Minion m : enemyBoard.getMinionsOnBoard()){
-                        m.minionDeath(m, enemyBoard.getMinionsOnBoard());
+                for (Minion m : enemyBoard.getMinionsOnBoard()) {
+                    m.minionDeath(m, enemyBoard.getMinionsOnBoard());
+                }
+                ui.displayMessage("You use Flamestrike and deal 4 damage to all enemy minion.");
+                break;
+
+            //Hunter spells
+            case "Arcane Shot":
+                myBoard.fireballAny(2, enemyBoard);
+                ui.displayMessage("Your target is hit by Arcane Shot and loses 2 hp.");
+                break;
+            case "Animal Companion":
+                Random ran1 = new Random();
+                int randomCompanion;
+
+                for (int i = 0; i < 3; i++) {
+                    ranNum = ran1.nextInt(enemyBoard.getMinionsOnBoard().size() + 1);
+                    switch (randomCompanion) {
+                        case 0:
+                            if (myBoard.getMinionsOnBoard().size() < 7) {
+                                Minion animalCompanion = new Minion("Animal Companion", 3, 4, 3);
+                                myBoard.getMinionsOnBoard().add(animalCompanion);
+                                ui.displayMessage("You summon a 4/3 BJØRN.");
+                            } else {
+                                ui.displayMessage("Not enough space on board for a companion");
+                            }
+                            break;
+                        case 1:
+                            if (myBoard.getMinionsOnBoard().size() < 7) {
+                                Minion animalCompanion2 = new Minion("Animal Companion", 3, 2, 4);
+                                myBoard.getMinionsOnBoard().add(animalCompanion2);
+                                ui.displayMessage("You summon a 2/4 Monkey.");
+                            } else {
+                                ui.displayMessage("Not enough space on board for a companion");
+                            }
+
+                            break;
+                        case 2:
+                            if (myBoard.getMinionsOnBoard().size() < 7) {
+                                Minion animalCompanion3 = new Minion("Animal Companion", 3, 2, 4);
+                                myBoard.getMinionsOnBoard().add(animalCompanion3);
+                                ui.displayMessage("You summon a 4/2 Boar.");
+                            } else {
+                                ui.displayMessage("Not enough space on board for a companion");
+                            }
+                            break;
+
+                        default:
+                            System.out.println("Spell missing in SpellEffect");
+                            break;
+
                     }
-                    ui.displayMessage("You use Flamestrike and deal 4 damage to all enemy minion.");
-                break;
-            default:
-                System.out.println("Spell missing in SpellEffect");
-                break;
+
+
+                }
+
+
+                public Minion enemyOrFriendlyMinion (Board myBoard, Board enemyBoard, TextUI ui){
+                ui.displayMessage("1. Enemy minion \n 2. Friendly minion \n");
+                Minion minion = null;
+                switch (ui.getInput()) {
+                    case "1":
+                        minion = myBoard.pickMinion(enemyBoard.getMinionsOnBoard());
+                        break;
+                    case "2":
+                        minion = myBoard.pickMinion(myBoard.getMinionsOnBoard());
+                        break;
+                    default:
+                        return enemyOrFriendlyMinion(myBoard, enemyBoard, ui);
+                }
+                return minion;
+            }
+
 
         }
-
-
-    }
-
-
-    public Minion enemyOrFriendlyMinion(Board myBoard, Board enemyBoard, TextUI ui) {
-        ui.displayMessage("1. Enemy minion \n 2. Friendly minion \n");
-        Minion minion = null;
-        switch (ui.getInput()){
-            case "1":
-                minion = myBoard.pickMinion(enemyBoard.getMinionsOnBoard());
-                break;
-            case "2":
-                minion = myBoard.pickMinion(myBoard.getMinionsOnBoard());
-                break;
-            default:
-                return enemyOrFriendlyMinion(myBoard, enemyBoard, ui);
-        }
-        return minion;
-    }
-
-
-
-   }
 
 
