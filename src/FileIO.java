645d47cd6c51de23ba5eaf0987c4f7acbba2bd54
fmoes <cph-fm124@cphbusiness.dk>
2023-12-17 02:05:37 +0100

    Rettelse i txt filer og fileio

--- src/FileIO.java
+++ src/FileIO.java
@@ -18,12 +18,25 @@ public class FileIO implements FileEditor {
 
                 switch (cardType){
                     case "Minion":
-                        String cardName = row[1];
-                        int cardCost = Integer.parseInt(row[2].split(": ")[1]);
+                        String cardName1 = row[1];
+                        int cardCost1 = Integer.parseInt(row[2].split(": ")[1]);
                         int minionAttack = Integer.parseInt(row[3].split(": ")[1]);
                         int minionMaxHealth = Integer.parseInt(row[4].split(": ")[1]);
-                        Minion minion = new Minion(cardName,cardCost,minionAttack,minionMaxHealth);
+                        Minion minion = new Minion(cardName1,cardCost1,minionAttack,minionMaxHealth);
                         data.add(minion);
+                        break;
+                    case "Spell":
+                        String cardName2 = row[1];
+                        int cardCost2 = Integer.parseInt(row[2].split(": ")[1]);
+                        Spell spell = new Spell(cardName2,cardCost2);
+                        data.add(spell);
+                        break;
+                    case "Weapon":
+                        String cardName3 = row[1];
+                        int cardCost3 = Integer.parseInt(row[2].split(": ")[1]);
+                        int weaponAttack = Integer.parseInt(row[3].split(": ")[1]);
+                        int weaponDurability= Integer.parseInt(row[4].split(": ")[1]);
+                        Weapon weapon = new Weapon(cardName3,cardCost3,weaponAttack,weaponDurability);
                         break;
                     default:
                         System.out.println("Invalid card type");
